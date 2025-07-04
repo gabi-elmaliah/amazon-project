873e3bbb81814220448a3124fb35af878779fd24
gabi-elmaliah <gabbyelmaliah@gmail.com>
2025-07-04 13:43:05 +0100

    added tests

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart, addToCart,calculateCartQuantity} from '../data/cart.js';
+
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -61,15 +62,16 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = calculateCartQuantity();
-  
-  if(cartQuantity==0){
-    cartQuantity="";
-  }
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
   document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity;
 }
-updateCartQuantity();
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
